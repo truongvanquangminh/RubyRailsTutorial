88867bfc3e679e9793979f7c13d35b1dbb3dee85
Quang Minh <minhtruong20002@gmail.com>
2022-05-19 22:11:08 +0700

    Chapter_7

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/signup', to: 'users#new'
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
